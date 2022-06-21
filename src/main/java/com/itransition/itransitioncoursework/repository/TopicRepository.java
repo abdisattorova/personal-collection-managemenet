147631e6ae794e6dc6b14b5f20d769d829f1d9dc
Sevinch Abdisattarova <abdisattarovasevinch5@gmail.com>
2022-06-21 16:06:48 +0500

    collecton creat

---
+++ src/main/java/com/itransition/itransitioncoursework/repository/TopicRepository.java
@@ -0,0 +1,11 @@
+package com.itransition.itransitioncoursework.repository;
+
+import com.itransition.itransitioncoursework.entity.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic,UUID> {
+}
